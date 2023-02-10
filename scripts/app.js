d3d8163bb8732bef7fc68ddfdcbe282c6be01279
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-10 11:25:53 +0600

    Loading and saving data

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+/* utils */
+function loadData() {
+    const habbitsString = localStorage.getItem(HABBIT_KEY);
+    const habbitArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
+    }
+}
+
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+(() => {
+    loadData();
+})();
