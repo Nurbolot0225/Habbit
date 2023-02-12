5d823e48cf9adaa056a931469782901155c49964
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-12 22:35:16 +0600

    deleting a day

--- scripts/app.js
+++ scripts/app.js
@@ -71,7 +71,7 @@ function rerenderContent(activeHabbit) {
         element.classList.add('habbit');
         element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
             <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
-            <button class="habbit__delete">
+            <button class="habbit__delete" onclick="deleteDay(${index})">
                 <img src="images/Delete.svg" alt="Удалить день ${Number(index) + 1}">
             </button>`;
         page.content.daysContainer.appendChild(element);
@@ -114,9 +114,23 @@ function addDays(event) {
     saveData();
 }
 
+function deleteDay(index) {
+    habbits = habbits.map(habbit => {
+        if (habbit.id === globalactiveHabbitId) {
+            habbit.days.splice(index, 1);
+            return {
+                ...habbit,
+                days: habbit.days
+            };
+        }
+        return habbit;
+    });
+    rerender(globalactiveHabbitId);
+    saveData();
+}
+
 /* init */
 (() => {
     loadData();
     rerender(habbits[0].id);
 })();
-
